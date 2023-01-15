9f667f60e9c32a0dad8c41d9c1d062de8503a41d
Gary <gary@deken.net>
2023-01-15 10:06:17 -0600

    Update to use StaticAnimation

--- core/src/org/abberkeep/game/tutorial/ImageScreen.java
+++ core/src/org/abberkeep/game/tutorial/ImageScreen.java
@@ -6,6 +6,8 @@ package org.abberkeep.game.tutorial;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import org.abberkeep.gameframework.animation.StaticAnimation;
+import org.abberkeep.gameframework.animation.imagefx.CropEffect;
 import org.abberkeep.gameframework.screen.BaseScreen;
 
 /**
@@ -20,6 +22,10 @@ import org.abberkeep.gameframework.screen.BaseScreen;
  */
 public class ImageScreen extends BaseScreen {
    private Texture img;
+   private StaticAnimation sa1;
+   private StaticAnimation sa2;
+   private StaticAnimation sa3;
+   private float size = 64;
 
    public ImageScreen(int width, int height) {
       super(width, height);
@@ -29,23 +35,33 @@ public class ImageScreen extends BaseScreen {
    @Override
    public void show() {
       img = getTexture("DemoPortait.png");
+      sa1 = new StaticAnimation(img);
+      sa2 = new StaticAnimation(img, 256, 256);
+      CropEffect ci = new CropEffect(15, 5, 100, 105);
+      sa3 = new StaticAnimation(img);
+      sa3.setImageFX(ci);
    }
 
    @Override
    protected void renderChild(float deltaTime) {
       float y = 20;
       // draws the image.
-      batch.draw(img, 0, y);
+      sa1.draw(batch, 0, y);
       // draw resizing of the image
+      //sa2.draw(batch, 150, y);
+//      size += deltaTime;
+//      sa2.resize(size, size);
       //batch.draw(img, 150, y, 256, 256);
       // batch.draw(img, 150, y, 100, 200);
       // draw a region of the image
       //batch.draw(img, 150, y, 15, 15, 128, 128);
+      sa3.draw(batch, 150, y);
       //batch.draw(img, 150, y, 15, 5, 100, 105);
       // zoom in on portion of the image
       //batch.draw(img, 150, y, 128, 128, 0f, 1f, 1f, 0f);
       //batch.draw(img, 150, y, 128, 128, 0f, .75f, 1f, .25f);
-      batch.draw(img, 150, y, 128, 128, 0f, 1.75f, 1f, .25f);
+      //sa4.draw(batch, 300, y);
+      //batch.draw(img, 150, y, 128, 128, 0f, 1.75f, 1f, .25f);
       //batch.draw(img, 150, y, 30, 35, 0.3f, .5f, .45f, .35f);
       //batch.draw(img, 150, y, 128, 128, 1f, 0f, 0f, 1f);
       // draw resizing, portion and flipping.
@@ -57,9 +73,8 @@ public class ImageScreen extends BaseScreen {
       //batch.setColor(Color.RED);
       //batch.draw(img, 150, y);
       //batch.setColor(Color.CLEAR);
-      batch.setColor(Color.WHITE);
-      batch.draw(img, 300, y);
-
+      //batch.setColor(Color.WHITE);
+      //batch.draw(img, 300, y);
    }
 
 }
